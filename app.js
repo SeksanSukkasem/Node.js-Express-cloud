e86874462a9a2c436b7c3b033d30e43c7073cc33
SeksanSukkasem <143654703+SeksanSukkasem@users.noreply.github.com>
2024-05-10 22:14:00 +0700

    update

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require("path");
+const nodemon = require('nodemon');
+
 
 const app = express();
 const port = process.env.port || 5000;
 
 app.use(morgan('combined'));
 
-app.use(express.static(path.join(__dirname,"./public/")))
+// app.use(express.static(path.join(__dirname,"./public/")))
+app.set("views","./src/views");
+app.set("view engine","ejs");
 
-// app.get("/",(req,res)=> {
-//     res.send('Hello Hello');
-// });
+app.get("/",(req,res)=> {
+    res.render('index',{username: 'Seksan+' , customer:['Kiti','seksan']});
+});
 
 app.listen(port, async ()=> {
     const chalk = await import('chalk');
-    debug("Listening on port"+ chalk.default.red(port));
+    debug("Listening on port"+ chalk.default.green(port));
 });
 
