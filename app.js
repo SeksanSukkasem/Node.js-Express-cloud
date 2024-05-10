7f0a8b4368ba390b9acc1dd618e6b8ed55baa58b
SeksanSukkasem <143654703+SeksanSukkasem@users.noreply.github.com>
2024-05-10 22:35:41 +0700

    UPdate

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require("path");
 const nodemon = require('nodemon');
-
+const productRouter = express.Router();
 
 const app = express();
 const port = process.env.port || 5000;
 
 app.use(morgan('combined'));
 
+productRouter.route("/").get((req,res) => {
+    res.send("Hello Product");
+})
+
+
+productRouter.route("/1").get((req,res) => {
+    res.send("Hello Product 1 ");
+})
+
+
+app.use('/Products', productRouter);
+
 app.use(express.static(path.join(__dirname,"./public/")))
 app.set("views","./src/views");
 app.set("view engine","ejs");
+
 
 app.get("/",(req,res)=> {
     res.render('index',{username: 'Seksan+' , customer:['Kiti','seksan']});
