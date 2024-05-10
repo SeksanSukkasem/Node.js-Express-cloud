4de7bff43a9aab0d99c103bd774e83a2dcd9b0bf
SeksanSukkasem <143654703+SeksanSukkasem@users.noreply.github.com>
2024-05-10 18:15:54 +0700

    Update

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
 
-app.get("/",(req,res)=> {
-    res.send('Hello Hello');
-});
+app.use(express.static(path.join(__dirname,"./public/")))
+
+// app.get("/",(req,res)=> {
+//     res.send('Hello Hello');
+// });
 
 app.listen(port, async ()=> {
     const chalk = await import('chalk');
